b61aa040a852cc375919139b2815aa7278094e25
Gonzalo Fernandez <gfernand@c3r13s4.42madrid.com>
2022-05-10 16:23:56 +0200

    avanti 5

--- fdf.c
+++ fdf.c
@@ -6,7 +6,7 @@
 /*   By: gfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 14:39:23 by gfernand          #+#    #+#             */
-/*   Updated: 2022/05/09 16:59:58 by gfernand         ###   ########.fr       */
+/*   Updated: 2022/05/10 15:00:23 by gfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,31 @@
 
 int	main(int argc, char **argv)
 {
-	int	fd;
-	void	*mlx_ptr;
-	void	*win_ptr;
+	int		fd;
+	t_data	data;
 
 	if (argc != 2)
 		ft_putfinish("WRONG PARAMETERS\n");
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 0)
 		ft_putfinish("WRONG MAP");
-	mlx_ptr = mlx_init();
-	win_ptr = mlx_new_window(mlx_ptr, 600, 300, "window");
-	if (win_ptr == NULL)
+	data.mlx_ptr = mlx_init();
+	data.win_ptr = mlx_new_window(data.mlx_ptr, 900, 600, "FDF");
+	if (data.win_ptr == NULL)
 	{
-		free(win_ptr);
+		free(data.win_ptr);
 		ft_putfinish("WRONG WINDOW\n");
 	}
-	while (1)
-		;
-	mlx_destroy_window(mlx_ptr, win_ptr);
-	free(mlx_ptr);
+	mlx_loop_hook(data.mlx_ptr, &ft_pixel, &data);
+	mlx_loop(data.mlx_ptr);
+	mlx_destroy_window(data.mlx_ptr, data.win_ptr);
+	free(data.mlx_ptr);
+	return (0);
+}
+
+int	ft_pixel(t_data *data)
+{
+	if (data->win_ptr)
+		mlx_pixel_put(data->mlx_ptr, data->win_ptr, 450, 300, 0xFF0000);
 	return (0);
 }
--- fdf.h
+++ fdf.h
@@ -6,7 +6,7 @@
 /*   By: gfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/02 14:55:35 by gfernand          #+#    #+#             */
-/*   Updated: 2022/05/09 15:44:52 by gfernand         ###   ########.fr       */
+/*   Updated: 2022/05/10 15:00:26 by gfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,14 @@
 # include <unistd.h>
 # include <mlx.h>
 
+typedef struct s_data
+{
+	void	*mlx_ptr;
+	void	*win_ptr;
+}	t_data;
 void		ft_putfinish(char *str);
 char		**ft_split(char *s, char c);
 char		*ft_substr(char *s, int start, int len);
+int			ft_pixel(t_data *data);
 
 #endif
