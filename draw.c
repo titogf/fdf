303a17c2529d082577f47c9208c4bf127cedd1ba
Gonzalo Fa <gon@MacBook-Air-de-Gonzalo.local>
2022-10-13 13:53:53 +0200

    get out isometric funcion

--- draw.c
+++ draw.c
@@ -12,7 +12,6 @@
 
 #include "fdf.h"
 
-//static void	ft_isometric(t_data *data, int mx, int my, int n);
 static void	ft_bresenham(t_data *data, int mx, int my);
 static void	ft_vertical(t_data *data);
 
@@ -21,43 +20,12 @@ void	ft_putpixel(t_data *data, int mx, int my)
 	double	x;
 	double	y;
 
-	x = data->brsh.x0 - data->brsh.y0 - data->height[my][mx];
-	y = -data->height[my][mx] + (data->brsh.y0 + data->brsh.x0);
-	x = x * cos(0.523599) + data->posx;
-	y = y * sin(0.523599) + data->posy;
-	//x = data->brsh.x0 + data->posx;
-	//y = data->brsh.y0 + data->posy;
+	x = (data->brsh.x0 - data->brsh.y0) + data->posx;
+	y = (data->brsh.y0 + data->brsh.x0) / 2 + data->posy;
+//	x = data->brsh.x0 + data->posx;
+//	y = data->brsh.y0 + data->posy;
 	mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, data->color[my][mx]);
 }
-
-/*static void	ft_isometric(t_data *data, int mx, int my, int n)
-{
-	double	x;
-	double	y;
-	double	cs;
-	double	sen;
-	int	space;
-
-	space = data->location;
-	cs = cos(0.523599);
-	sen = sin(0.523599);
-	if (n == 0)
-	{
-		x = data->brsh.x0 - data->brsh.y0;
-		x = x - data->height[my][mx] * space;
-		y = -data->height[my][mx] + (data->brsh.y0 + data->brsh.x0);
-		data->brsh.x0 = x * cs + data->posx;
-		data->brsh.y0 = y * sen + data->posy;
-	}
-	else
-	{
-		x = data->brsh.x1 - data->brsh.y1;
-		x = x - data->height[my][mx + 1] * space;
-		y = -data->height[my][mx + 1] + (data->brsh.y1 + data->brsh.x1);
-		data->brsh.x1 = x * cs + data->posx;
-		data->brsh.y1 = y * sen + data->posy;
-	}
-}*/
 
 void	ft_horizontal(t_data *data)
 {
@@ -103,8 +71,7 @@ static void	ft_vertical(t_data *data)
 			data->brsh.y0 = (my - data->height[my][mx]) * space;
 			data->brsh.x1 = data->brsh.x0;
 			data->brsh.y1 = (my + 1 - data->height[my + 1][mx]) * space;
-			if (my != data->rows - 1)
-				ft_bresenham(data, mx, my);
+			ft_bresenham(data, mx, my);
 			my++;
 		}
 		mx++;
