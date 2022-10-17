b90648e2e3ab9bb86310d3cb42277f70bba50a37
Gonzalo Fa <gon@MacBook-Air-de-Gonzalo.local>
2022-10-17 14:24:03 +0200

    make isomeric

--- draw.c
+++ draw.c
@@ -20,11 +20,10 @@ void	ft_putpixel(t_data *data, int mx, int my)
 	double	x;
 	double	y;
 
-	x = (data->brsh.x0 - data->brsh.y0) * cos(0.5249 + data->posx;
-	x = (data->brsh.x0 - data->brsh.y0) - data->location * data->height[my][mx] + data->posx;
-	y = (data->brsh.y0 + data->brsh.x0) * sin(0.524) + data->posy;
-	//x = data->brsh.x0 + data->posx;
-	//y = data->brsh.y0 + data->posy;
+	x = (data->brsh.x0 - data->brsh.y0) * cos(0.5249) + data->posx;
+	y = -data->height[my][mx] * data->location + (data->brsh.y0 + data->brsh.x0) * sin(0.524) + data->posy;
+	//x = data->brsh.x0;
+	//y = data->brsh.y0;
 	mlx_pixel_put(data->mlx_ptr, data->win_ptr, x, y, data->color[my][mx]);
 }
 
@@ -44,9 +43,9 @@ void	ft_horizontal(t_data *data)
 		while (mx < data->columns - 1)
 		{
 			data->brsh.x0 = mx * space;
-			data->brsh.y0 = (my - data->height[my][mx]) * space;
+			data->brsh.y0 = my * space;
 			data->brsh.x1 = (mx + 1) * space;
-			data->brsh.y1 = (my - data->height[my][mx + 1]) * space;
+			data->brsh.y1 = my * space;
 			ft_bresenham(data, mx, my);
 			mx++;
 		}
@@ -69,9 +68,9 @@ static void	ft_vertical(t_data *data)
 		while (my < data->rows - 1)
 		{
 			data->brsh.x0 = mx * space;
-			data->brsh.y0 = (my - data->height[my][mx]) * space;
+			data->brsh.y0 = my * space;
 			data->brsh.x1 = data->brsh.x0;
-			data->brsh.y1 = (my + 1 - data->height[my + 1][mx]) * space;
+			data->brsh.y1 = (my + 1) * space;
 			ft_bresenham(data, mx, my);
 			my++;
 		}
