1aeb6ec0cd688dfd8b1cb7fe6eeda725b2507b71
Gonzalo Fernandez <gfernand@c3r14s1.42madrid.com>
2022-10-24 14:30:09 +0200

    avanti

--- draw.c
+++ draw.c
@@ -6,7 +6,7 @@
 /*   By: gfernand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/20 13:25:31 by gfernand          #+#    #+#             */
-/*   Updated: 2022/10/24 13:57:05 by gfernand         ###   ########.fr       */
+/*   Updated: 2022/10/24 14:29:52 by gfernand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -69,20 +69,20 @@ static void	ft_isometric(t_data *data, int mx, int my, int n)
 		data->brsh.x0 = mx * data->space;
 		data->brsh.y0 = my * data->space;
 		z = data->height[my][mx] * data->space;
-		x = (data->brsh.x0 - data->brsh.y0) * data->brsh.cs + data->posx;
-		y = -z + (data->brsh.y0 + data->brsh.x0) * data->brsh.sn + data->posy;
-		data->brsh.x0 = x;
-		data->brsh.y0 = y;
+		x = (data->brsh.x0 - data->brsh.y0) * data->brsh.cs;
+		y = -z + (data->brsh.y0 + data->brsh.x0) * data->brsh.sn;
+		data->brsh.x0 = x + data->posx;
+		data->brsh.y0 = y + data->posy;
 	}
 	else if (n == 1)
 	{
 		data->brsh.x1 = mx * data->space;
 		data->brsh.y1 = my * data->space;
 		z = data->height[my][mx] * data->space;
-		x = (data->brsh.x1 - data->brsh.y1) * data->brsh.cs + data->posx;
-		y = -z + (data->brsh.y1 + data->brsh.x1) * data->brsh.sn + data->posy;
-		data->brsh.x1 = x;
-		data->brsh.y1 = y;
+		x = (data->brsh.x1 - data->brsh.y1) * data->brsh.cs;
+		y = -z + (data->brsh.y1 + data->brsh.x1) * data->brsh.sn;
+		data->brsh.x1 = x + data->posx;
+		data->brsh.y1 = y + data->posy;
 	}
 }
 
